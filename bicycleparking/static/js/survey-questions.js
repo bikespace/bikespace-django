0219b0ec9cc94afe4dd05b1c4219cafda29d60d1
Tessa Thornton <tessathornton@gmail.com>
2017-09-20 00:33:28 +0000

    Multi question pages

--- bicycleparking/static/js/survey-questions.js
+++ bicycleparking/static/js/survey-questions.js
@@ -2,80 +2,96 @@ import types from './types/types';
 
 const questions = [
   {
-    key: 'target_location',
-    type: types.LATLNG,
-    heading: 'Where were you trying to go?',
-    text: 'Tell us where you were trying to go or where you tried to park',
-    required: true
-  },
-  {
-    key: 'problem_type',
-    type: types.ARRAY,
-    heading: 'What was the problem here?',
-    text: 'select all that apply',
-    required: true,
-    values: [
+    heading: 'What problem(s) prevented you from parking your bike?',
+    questions: [
       {
-        key: 'full',
-        text: 'All nearby bike racks were full'
+        key: 'problem_type',
+        type: types.ARRAY,
+        heading: 'What was the problem here?',
+        text: 'select all that apply',
+        required: true,
+        values: [
+          {
+            key: 'full',
+            text: 'All nearby bike racks were full'
+          },
+          {
+            key: 'difficult',
+            text: 'Difficult to locate bike racks'
+          },
+          {
+            key: 'absent',
+            text: 'No nearby bike racks'
+          }
+        ]
       },
       {
-        key: 'difficult',
-        text: 'Difficult to locate bike racks'
+        key: 'photo',
+        type: types.IMAGE,
+        heading: 'Upload a picture',
+        text: 'Optionally upload an image of a problem'
       },
-      {
-        key: 'absent',
-        text: 'No nearby bike racks'
-      }
     ]
   },
   {
-    key: 'report_time',
-    type: types.DATETIME,
-    required: true,
-    heading: 'When did you identify the problem?',
-    text: 'Specify the time and date',
-    default: new Date() 
-  },
-  {
-    key: 'duration',
-    type: types.STRING,
-    heading: 'How long were you planning to lock your bike?',
-    required: true,
-    text: '',
-    values: [
+    heading: 'Where were you going?',
+    questions: [
       {
-        key: 'short',
-        text: 'Less than 1 hour'
+        key: 'target_location',
+        type: types.LATLNG,
+        text: 'Tell us where you were trying to go or where you tried to park',
+        required: true
       },
-      {
-        key: 'med',
-        text: 'Up to 8 hours'
-      },
-      {
-        key: 'long',
-        text: 'Overnight'
-      }
     ]
   },
   {
-    key: 'comment',
-    type: types.TEXT,
+    heading: 'when did you experience this?',
+    questions: [
+      {
+        key: 'report_time',
+        type: types.DATETIME,
+        required: true,
+        heading: 'When did you identify the problem?',
+        text: 'Specify the time and date',
+        default: new Date() 
+      },
+      {
+        key: 'duration',
+        type: types.STRING,
+        heading: 'How long were you planning to lock your bike?',
+        required: true,
+        text: '',
+        values: [
+          {
+            key: 'short',
+            text: 'Less than 1 hour'
+          },
+          {
+            key: 'med',
+            text: 'Up to 8 hours'
+          },
+          {
+            key: 'long',
+            text: 'Overnight'
+          }
+        ]
+      },
+    ]
+  },
+  {
     heading: 'Anything else?',
-    text: 'Please leave a short comment',
-  },
-  {
-    key: 'photo',
-    type: types.IMAGE,
-    heading: 'Upload a picture',
-    text: 'Optionally upload an image of a problem'
-  },
-  {
-    key: 'email',
-    type: types.STRING,
-    heading: 'Let\'s keep in touch',
-    text: 'Sign up to receive updates'
+    questions: [{
+      key: 'comment',
+      type: types.TEXT,
+      text: 'Please leave a short comment',
+    },
+    {
+      key: 'email',
+      type: types.STRING,
+      text: 'Sign up to receive updates'
+    }]
   }
+
 ]
 
 export default questions;

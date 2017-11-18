0dc6935e4e3307f68925f2b45f65d45665678560
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-11-18 14:35:00 -0500

    keeping postgres service name

--- Bicycle_parking/settings/environments/development.py
+++ Bicycle_parking/settings/environments/development.py
@@ -1,21 +1,6 @@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-#please fill out these settings for your own local machine!
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bike_parking_toronto',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
 S3_BUCKET = ''

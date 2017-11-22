ac332cd2fe1fa621907d80d678b9a0b90244aff4
Yan MATAGNE <ymatagne@norsys.fr>
2017-11-22 14:30:54 -0500

    feat(upload) : add upload to S3

--- bicycleparking/views.py
+++ bicycleparking/views.py
@@ -11,10 +11,14 @@ from rest_framework import status
 
 from bicycleparking.serializers import SurveyAnswerSerializer
 from bicycleparking.models import SurveyAnswer
+from bicycleparking.uploader import Uploader
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'bicycleparking/landing.html', {})
+
 
 class SurveyAnswerList(generics.CreateAPIView):
     queryset = SurveyAnswer.objects.all()
@@ -23,14 +27,14 @@ class SurveyAnswerList(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(ip=self.request.META['REMOTE_ADDR'])
 
+
 class UploadPicture(APIView):
     parser_classes = (FileUploadParser,)
     renderer_classes = (JSONRenderer, )
+    uploader = Uploader()
 
     def put(self, request, filename, format=None):
         file_obj = request.data['file']
-        # ...
-        # do some stuff with uploaded file
-        # ...
-        content = {'s3_name': "toto"}
+        print(request.data)
+        content = {'s3_name': self.uploader.toS3(filename, file_obj)}
         return Response(content)

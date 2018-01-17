55d4ea0846f1b6bdef64f0405db388d3f1ee10bb
John <john.spragge@manifoldeye.ca>
2018-01-16 19:43:16 -0500

    first erase test databases (if any)

---
+++ test/removeTestDB.py
@@ -0,0 +1,38 @@
+import psycopg2
+import sys
+import xml.etree.ElementTree as ET
+
+DB_HOST=os.getenv ('BIKE_DB_HOST', 'localhost')
+DB_USER=os.getenv ('BIKE_DB_USER', 'postgres')
+DB_PW=os.getenv ('BIKE_DB_PW', '')
+
+TO_DELETE = ("test_bike_parking_toronto", "test_intersection")
+
+for dbn in TO_DELETE :
+  try:
+     connection = psycopg2.connect ("dbname='intersection' host='{0}' user = '{1}' password = '{2}'".format (DB_HOST, DB_USER, DB_PW))
+     connection.autocommit = True
+     cursor = connection.cursor ()
+     print ("deleting database {0}".format (dbn))
+     cursor.execute ("drop database if exists {0};".format (dbn))
+     cursor.close ()
+     connection.close ()
+  except psycopg2.Warning as warning:
+     if warning.pgerror != None: 
+        print (warning.pgerror)
+     else:
+        try :
+           et = errorcodes.lookup(warning.pgcode)
+           print (et)
+        except KeyError as secondaryError:
+           print ("unknown warning in postgres link")
+  except psycopg2.Error as error:
+     if error.pgerror != None: 
+        print (error.pgerror)
+     else:
+        try :
+           et = errorcodes.lookup(warning.pgcode)
+           print (et)
+        except KeyError as secondaryError:
+           print ("unknown error in postgres link")
+

86e425b317c9bc615133b8ef8a17994784424a45
Yan MATAGNE <ymatagne@norsys.fr>
2018-01-18 18:08:13 -0500

    fix(database): add password

--- Bicycle_parking/settings/components/database.py
+++ Bicycle_parking/settings/components/database.py
@@ -9,7 +9,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bike_parking_toronto',
         'USER': os.getenv ('BIKE_DB_USER', 'postgres'),
-        #'PASSWORD': os.getenv ('BIKE_DB_PW', 'bike_parking_toronto'),
+        'PASSWORD': os.getenv ('BIKE_DB_PW', ''),
         'HOST': os.getenv ('BIKE_DB_HOST', 'postgres'),
         'PORT': '5432',
     },
@@ -17,7 +17,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'intersection',
         'USER': os.getenv ('BIKE_DB_USER', 'postgres'),
-        #'PASSWORD': os.getenv ('BIKE_DB_PW', 'bike_parking_toronto'),
+        'PASSWORD': os.getenv ('BIKE_DB_PW', ''),
         'HOST': os.getenv ('BIKE_DB_HOST', 'postgres'),
         'PORT': '5432',
     }

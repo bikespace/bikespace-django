7306856fc14740716546ba02e9a99cab6f466fcf
Yan MATAGNE <ymatagne@norsys.fr>
2018-02-08 21:06:25 -0500

    fix(dashboard) : style

--- bicycleparking/static/js/contents/summary.js
+++ bicycleparking/static/js/contents/summary.js
@@ -19,8 +19,24 @@ export default class Summary extends Content {
       <div class="screen1">
         <div class="progress prog5"></div>
         <h1>${this.props.heading}</h1>
-        <h2>${this.props.text}</h2>
-
+        <div class="summary">
+          <h1 class="titleSummary">Problems</h1>
+          <div class="options">
+              <li><em>dqsdqsdsqdsqdqsdqsdsqdqsdq</em></li>
+          </div>
+          <h1 class="titleSummary">Date & Time</h1>
+          <div class="options">
+              <li><em>January 25, 2018 at 3:30 PM</em></li>
+          </div>
+          <div>
+            <div class="mapSummary">
+              <h1 class="titleSummary">Location</h1>
+            </div>            
+            <div class="pictureSummary">
+              <h1 class="titleSummary">Photo</h1>
+            </div>
+          </div>
+        </div>        
       </div>
       `
     )

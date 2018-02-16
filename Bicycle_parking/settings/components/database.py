e39fc54a4d76f6f71fd0dbb53b4299b93f3f5d44
John <john.spragge@manifoldeye.ca>
2018-02-15 19:24:51 -0500

    setups and removals

--- Bicycle_parking/settings/components/database.py
+++ Bicycle_parking/settings/components/database.py
@@ -9,16 +9,16 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bike_parking_toronto',
         'USER': os.getenv('BIKE_DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('BIKE_DB_PW', 'postgres'),
-        'HOST': os.getenv('BIKE_DB_HOST', 'postgres'),
+        'PASSWORD': os.getenv('BIKE_DB_PW', ''),
+        'HOST': os.getenv('BIKE_DB_HOST', 'localhost'),
         'PORT': '5432',
     },
     'geospatial': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'intersection',
         'USER': os.getenv('BIKE_DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('BIKE_DB_PW', 'postgres'),
-        'HOST': os.getenv('BIKE_DB_HOST', 'postgres'),
+        'PASSWORD': os.getenv('BIKE_DB_PW', ''),
+        'HOST': os.getenv('BIKE_DB_HOST', 'localhost'),
         'PORT': '5432',
     }
 }

186496f44d9929f2b34928058861cafb80227f91
John <john.spragge@manifoldeye.ca>
2018-02-20 15:08:29 -0500

    throttle applied

--- Bicycle_parking/settings/components/base.py
+++ Bicycle_parking/settings/components/base.py
@@ -74,6 +74,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Bicycle_parking.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': 
+    (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': 
+    {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {

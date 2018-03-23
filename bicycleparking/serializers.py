a84a42918a2ef49f8d7518d5d0537ccbc02f2de9
John <john.spragge@manifoldeye.ca>
2018-03-22 20:54:38 -0400

    draft of serializer

--- bicycleparking/serializers.py
+++ bicycleparking/serializers.py
@@ -3,7 +3,14 @@ from django.contrib.postgres.fields import JSONField
 from bicycleparking.models import SurveyAnswer
 from bicycleparking.models import Picture
 
-class SurveyAnswerSerializer (serializers.ModelSerializer):
+class SurveyAnswerSerializer (serializers.ModelSerializer) :
     class Meta:
         model = SurveyAnswer
-        fields = ('latitude', 'longitude', 'survey', 'photo_uri')
+        fields = ('latitude', 'longitude', 'survey')
+#
+# class PictureSerializer (serializers.ModelSerializer) :
+#     photo_uri = serializers.CharField (allow_null = True, required = False, source = 'picture')
+# 
+#     class Meta :
+#         model = Picture
+#         fields = ('photo_uri', 'answer')

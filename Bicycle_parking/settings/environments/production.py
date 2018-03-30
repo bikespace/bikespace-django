f19a41c4338cf71537915c8c64ba635d929891c7
Sonal Ranjit <sonal.ranjit3@gmail.com>
2018-03-29 20:28:24 -0400

    testing collectstatic to s3

--- Bicycle_parking/settings/environments/production.py
+++ Bicycle_parking/settings/environments/production.py
@@ -5,3 +5,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 S3_BUCKET = os.environ.get('S3_BUCKET')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = os.environ.get('STATIC_URL')
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}

002a0b036028b01c0a00d2499d055ed5f26a33d0
Sonal Ranjit <sonal.ranjit3@gmail.com>
2018-03-29 21:31:05 -0400

    testing static url from s3

--- Bicycle_parking/settings/environments/production.py
+++ Bicycle_parking/settings/environments/production.py
@@ -9,7 +9,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN = os.environ.get('STATIC_URL')
-
+STATIC_URL = os.environ.get('S3_FULL_STATIC_URL')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }

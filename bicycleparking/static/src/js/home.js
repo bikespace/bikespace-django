691a0f2387d766e096245f21938b1cfae795d3b4
Yan MATAGNE <ymatagne@norsys.fr>
2018-06-09 14:43:52 -0400

    feqture(cach) : fixe the cach problem

--- bicycleparking/static/src/js/home.js
+++ bicycleparking/static/src/js/home.js
@@ -20,6 +20,34 @@ export default class Home {
     document.getElementById('start').addEventListener('click', (event) => {
       this.survey.router.navigate(`/start`);
     });
+    let btnAdd=  document.getElementById('btnAdd')
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      btnAdd.style.display = 'block';
+      console.log("hello")
+    });
+
+    btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
   }
 
   template() {
@@ -43,6 +71,10 @@ export default class Home {
                       <a id="start"><p><em>Report a parking issue</em></p></a>
                   </div>
               </div>
+
+          <div class="installPWA">
+            <button id="btnAdd"></button>
+          </div>
       </footer> 
       `
     )

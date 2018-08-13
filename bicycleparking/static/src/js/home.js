2f218debe951a0f5400fc1b3b2eae96dddbbca3d
dav-idcox <dav.idcox@yahoo.ca>
2018-08-13 19:45:49 -0400

    moved the progress bar so that it stays below the header, and made the BikeSpace logo on the home screen into a button

--- bicycleparking/static/src/js/home.js
+++ bicycleparking/static/src/js/home.js
@@ -63,6 +63,9 @@ export default class Home {
         </div>
           <div class="logo">
             <h1 class="maintitle tshadow">BikeSpace</h1>
+          </div>
+      
+          <div class="nav">
                   <div class="button bshadow">
                       <a><p><em>Report a parking issue</em></p></a>
                   </div>

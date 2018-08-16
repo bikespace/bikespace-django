bbab413d40784c3ba08074d45080ab383788fe26
John <john.spragge@manifoldeye.ca>
2018-08-16 18:14:57 -0400

    updated  moderation class

--- bicycleparking/Moderate.py
+++ bicycleparking/Moderate.py
@@ -15,8 +15,9 @@ from bicycleparking.models import SurveyAnswer, Event, Area, Intersection2d, App
 class Moderate (object):
   """Implements a moderation protocol for the pictures in the requests.""" 
 
-  def getUnmoderated (self) :
+  def getUnmoderated (self, userId) :
      """Implements an html interface to list unmoderated requests."""
+     self.getPictures 
 
   def getPictures (self) :
      """Returns a string representing the html links to display pictures and 
@@ -26,10 +27,19 @@ class Moderate (object):
      unmoderated = Event.objects.filter (approval = None)
      for event in unmoderated :
         link = event.answer.id
+        comment = event.answer.comments
         pictures = Picture.objects.filter (answer__id = link)
         formText = formText + '<div>'
         for pmod in pictures :
            formText = formText + '<img src="app/pictures/{}" width="80" height="100">'.format (pmod.photo_uri)
+        formText = formText + '<div><p><textarea>' + event.answer.comments + '</textarea></p></div>'
         formText = formText + '<button onclick="approve ({})">Approve</button>'.format (event.id)
-        print ("</div>")   
-        
+        formText = formText + "</div>"
+     formText = formText + "</form>"   
+
+  def approve (self, eventId, userId) :
+    """Records the moderated records in the approval table in the database, 
+    together with the identifier of the user who approved the request."""
+    event = Event.object.get (id = eventId)
+    approval = Approval (approved = event, moderatorId = userId)
+    approval.save ()

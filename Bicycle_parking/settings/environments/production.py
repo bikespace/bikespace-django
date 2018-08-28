b41c09bb970a75b38efb36462897ad8e926b4114
Yan MATAGNE <ymatagne@norsys.fr>
2018-08-27 20:49:55 -0400

    fix(sw): Fix SW problem ?

--- Bicycle_parking/settings/environments/production.py
+++ Bicycle_parking/settings/environments/production.py
@@ -8,7 +8,9 @@ S3_BUCKET = os.environ.get('S3_BUCKET')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, '../bicycleparking/static/dist'),
+]
 AWS_S3_CUSTOM_DOMAIN = os.environ.get('STATIC_URL')
 
 STATIC_URL = '/static/'

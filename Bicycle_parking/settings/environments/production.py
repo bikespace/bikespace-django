b575954d2a416814137024f4668c7e45b0b339f3
Yan MATAGNE <ymatagne@norsys.fr>
2018-08-27 21:41:52 -0400

    fix(style) : remove the dist folder - it s generated

--- Bicycle_parking/settings/environments/production.py
+++ Bicycle_parking/settings/environments/production.py
@@ -9,7 +9,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, '../bicycleparking/static/dist'),
+    os.path.join(PROJECT_ROOT, '../../bicycleparking/static/dist'),
 ]
 AWS_S3_CUSTOM_DOMAIN = os.environ.get('STATIC_URL')
 

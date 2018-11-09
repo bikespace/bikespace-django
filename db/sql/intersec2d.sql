8d6497911277835ee2eda6f76319079f8d711c5a
Sonal Ranjit <sonal.ranjit3@gmail.com>
2018-11-08 22:25:57 -0500

    WIP: moving into containerized postgres instance

---
+++ db/sql/intersec2d.sql
@@ -0,0 +1,7 @@
+﻿create table intersection2d 
+       as select distinct on (longitude, latitude)
+                 gid, int_id, intersec5, classifi6, classifi7, 
+                 longitude, latitude, objectid, geom
+          from centreline_intersection_wgs84
+          order by longitude, latitude, gid;
+alter table intersection2d add primary key (gid);
--- sql/intersection_types.sql
+++
@@ -1,3 +0,0 @@
-﻿select count (gid), classifi6, max(classifi7) from intersection2d
-   group by classifi6;
-select count (gid) from intersection2d;

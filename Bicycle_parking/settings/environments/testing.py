5b41288dce1b553bfb75c9ce7f4843736c94fc31
Sonal Ranjit <sonal.ranjit3@gmail.com>
2018-11-14 12:24:04 -0500

    update testiing env setttings

--- Bicycle_parking/settings/environments/testing.py
+++ Bicycle_parking/settings/environments/testing.py
@@ -1,6 +1,30 @@
 import dj_database_url
 import os
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+S3_BUCKET = ''
+
+AWS_STORAGE_BUCKET_NAME = ''
+
+DISABLE_COLLECTSTATIC = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, '../bicycleparking/static/dist'),
+]
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 

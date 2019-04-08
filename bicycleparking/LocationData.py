e216df2e41b0f76b04514cb7dc989178538c0f52
CodyGramlich <cody.gramlich@edmonton.ca>
2019-04-08 10:56:39 -0600

    Replaced get location from db to get location from Google API.

--- bicycleparking/LocationData.py
+++ bicycleparking/LocationData.py
@@ -20,22 +20,10 @@ class LocationData (object):
   """Encapsulates methods for accessing the geographical databases and 
   determining the closest of some type of intersection: any or major."""
 
-  latLimits = (43.58149, 43.886692)
-  longLimits = (-79.61179, -79.114705)
-  key = "*** reserved for future use"
-  majorSQL = """SELECT gid, int_id, intersec5, classifi6, classifi7, 
-                       longitude, latitude, objectid, geom,
-                       geom <-> st_setsrid(st_makepoint(%(long)s,%(lat)s),4326) as distance
-                FROM intersection2d
-                WHERE classifi6 = 'MJRSL' or classifi6 = 'MJRML'
-                ORDER BY distance
-                LIMIT 1;"""
-  closestSQL = """SELECT gid, int_id, intersec5, classifi6, classifi7, 
-                         longitude, latitude, objectid, geom,
-                         geom <-> st_setsrid(st_makepoint(%(long)s,%(lat)s),4326) as distance
-                  FROM intersection2d
-                  ORDER BY distance 
-                  LIMIT 1;"""
+  GEOCODE_BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+  MAPS_API_KEY = 'AIzaSyA73LhqHU5DzT-G7azXj6UAZ-z3MD8TmLY'
+  latLimits = (-90, 90)
+  longLimits = (-180, 180)
 
   def __init__ (self, latitude, longitude) :
      """Defines the local variables: only latitude and longitude are parameters."""
@@ -60,11 +48,15 @@ class LocationData (object):
   def getIntersectionNames (self) :
      """Derive a map with the names of the closest major and minor
      intersections."""
-     self.getIntersectionData ()
      result = {}
      if self.closest != None :
-        result ['closest']  = self.closest.intersec5 
-        result ['major'] = self.getMajor ().intersec5
+        if self.closest['status'] == 'OK':
+           for address in self.closest['results']:
+               for component in address['address_components']:
+                  if 'route' in component['types']:
+                     result['major'] = component['long_name']
+               if result:
+                  break
      return result
 
   def getDistance (self) :
@@ -113,33 +105,17 @@ class LocationData (object):
 
      try :
         if inLat and inLong :
-           result = self.lookupIntersection (LocationData.closestSQL, self.latitude, self.longitude)
+           result = self.lookupIntersection (self.latitude, self.longitude)
      except Exception as error:
         self.errors.append (error)
 
      return result
-
-  def getMajor (self) :
-      """Gets and returns the location of the major intersection nearest to the
-      closest intersection. This method determines whether the closest intersection 
-      is itself a major intersection. Ifso, it simply returns the identifier of the
-      closest intersection. If the closest intersection is not a major intersection,
-      it issues a request against the geographic database to find the nearest major
-      intersection to the current intersection and returns the resulting identifier. 
-      This method assumes a valid location input parameter; if the caller passes in 
-      an invalid location, the method will throw."""
-
-      if self.closest.classifi6 == 'MJRSL' or self.closest.classifi6 == 'MJRML' :
-         return self.closest
-      else :
-         return self.lookupIntersection (LocationData.majorSQL, 
-                                         self.closest.latitude, self.closest.longitude)
     
-  def lookupIntersection (self, sql, latt, longt) :
-      """Translates the selected data into a django data database request."""
-
-      location = {}
-      location ['lat'] = float (latt)
-      location ['long'] = float (longt)
-      query = Intersection2d.objects.raw (sql, location)
-      return query [0]
+  def lookupIntersection (self, lat, lng) :
+      """Translates the selected latitude and longitude into a Google Maps API reverse geocode response."""
+      payload = {
+         'latlng': "%s,%s" % (lat, lng),
+         'key': self.MAPS_API_KEY
+      }
+      resp = requests.get(self.GEOCODE_BASE_URL + '?', params=payload)
+      return resp.json()

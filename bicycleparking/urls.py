99dc5de433843795ba0eead651cef75143763e4d
CodyGramlich <cody.gramlich@edmonton.ca>
2019-04-15 11:50:25 -0600

    Removed CollectedData class and dashboarddata endpoint.

--- bicycleparking/urls.py
+++ bicycleparking/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     url(r'^api/upload/(?P<filename>[^/]+)$', views.UploadPicture.as_view()),
     url(r'^api/pictures/(?P<filename>[^/]+)$', views.DownloadPicture.as_view()),
     url(r'^api/location$', views.LocationNameRequest.as_view()),
-    url(r'^api/dashboarddata$', views.DashboardRequest.as_view ()),
     url(r'^sw.js', (TemplateView.as_view(template_name="bicycleparking/sw.js", content_type='application/javascript')), name='sw.js'),
     url(r'^manifest.json', (TemplateView.as_view(template_name="bicycleparking/manifest.json", content_type='application/json')), name='manifest.json'),
     url(r'^moderate_unapproved', views.submissions_to_moderate.as_view ()),

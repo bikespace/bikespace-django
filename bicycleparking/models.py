23a065e81aaa23849152fc94c11466746d42211b
CodyGramlich <cody.gramlich@edmonton.ca>
2019-04-17 15:55:04 -0600

    Removed street SurveyAnswer model.

--- bicycleparking/models.py
+++ bicycleparking/models.py
@@ -47,7 +47,6 @@ class SurveyAnswer(models.Model):
     longitude = models.FloatField()
     survey = JSONField(default=dict)
     comments = models.TextField(default=None, null=True)
-    street = models.TextField(default=None, null=True)
 
 class Picture (models.Model) :  
     """Contains the definition of a photograph uploaded by the user of the 

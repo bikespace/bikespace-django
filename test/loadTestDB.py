32777f970cae64817f402b575acd6d9ade220393
CodyGramlich <cody.gramlich@edmonton.ca>
2019-05-03 15:35:05 -0600

    loadTestDB.py now loads the test data with the new format.

--- test/loadTestDB.py
+++ test/loadTestDB.py
@@ -23,7 +23,7 @@ import os
 DB_HOST="127.0.0.1"
 DB_USER="postgres"
 DB_PW=""
-SINK_DB = "bike_parking_toronto"
+DB = "bike_parking_toronto"
 
 def flushTestDB (db) :
   """Erases all of the entries in the test tables."""
@@ -39,7 +39,7 @@ def flushTestDB (db) :
   db.commit ()
   cursor.close
 
-def construct (cursor, fileId, sink) :
+def construct (fileId, sink) :
   """Reads the definition of a set of transactions and inserts the 
      corresponding records into the test database."""
  
@@ -54,7 +54,7 @@ def construct (cursor, fileId, sink) :
 
   for test in entries : 
      test ['picture'] = random.choice (pictureList)
-     recordTransaction (test, cursor, sink)    
+     recordTransaction (test, sink)    
  
 def readTransactions (fn) :
   """Parses the xml input and extracts the list of elements defining the 
@@ -74,61 +74,17 @@ def processTransaction (transaction) :
      struc [field.tag] = field.text
   return struc
 
-def recordTransaction (transaction, cursor, sink) :
+def recordTransaction (transaction, sink) :
   """Creates an area specifier containing three 'place' elements describing the selected
   location, the major and the closest intersections, and writes it out as an area element to
   the result file. This method writes an entry to four tables: area, survey_answer, event
   and picture."""
-  majorSQL = """SELECT gid, int_id, intersec5, classifi6, classifi7, 
-                       longitude, latitude, objectid, geom,
-                       geom <-> st_setsrid(st_makepoint(%(longitude)s,%(latitude)s),4326) as distance
-                FROM intersection2d
-                WHERE classifi6 = 'MJRSL' or classifi6 = 'MJRML'
-                ORDER BY distance
-                LIMIT 1;"""
-  closestSQL = """SELECT gid, int_id, intersec5, classifi6, classifi7, 
-                         longitude, latitude, objectid, geom,
-                         geom <-> st_setsrid(st_makepoint(%(longitude)s,%(latitude)s),4326) as distance
-                  FROM intersection2d
-                  ORDER BY distance 
-                  LIMIT 1;"""
-
-  areas = {}
-  areas ['closest'] = locate (transaction, closestSQL, cursor)
-  areas ['major'] = locate (areas ['closest'], majorSQL, cursor)
 
   transaction ['ip'] = '127.0.0.1'
-  transaction ['area'] = put_area (areas, sink)
   transaction ['answer'] = put_survey_answer (transaction, sink)
-  transaction ['distance'] = areas ['closest']['distance']
 
   put_picture (transaction, sink)  
   put_event (transaction, sink)
-  
-def locate (transaction, sql, cursor) :
-  """Locates an intersection using the nearest location lookup function in
-     the posgresql geospatial database containing the intersection data. Takes
-     a preset location specifier and a postgresql cursor linking to the database"""
-
-  result = { }  
-  cursor.execute (sql, transaction)
-
-  row = cursor.fetchone ()
-  for item in zip (cursor.description, row):
-     name = item [0] [0]
-     value = item [1]
-     result [name] = value
-  return result
-  
-def put_area (elements, sink) :
-  """Puts the selected areas to the database."""
-  template = """insert into bicycleparking_area (closest, major) values 
-                (%(closest)s, %(major)s) returning id;"""
-  links = {}
-  links ['closest'] = elements ['closest'] ['gid']
-  links ['major'] = elements ['major'] ['gid']
-  sink.execute (template, links)
-  return sink.fetchone () [0]
 
 def put_survey_answer (transaction, sink) :
   """Puts the survey location data into the database."""
@@ -148,13 +104,11 @@ def makeSurveyJson (transaction) :
                "hours", "1-2hours"]
   problem_types = [ 'full', 'absent', 'damaged', 'badly', 'unusable', 'other']
 
-
   result = {}
   result ['map'] = [ [ float (transaction ['latitude']), float (transaction ['longitude']) ] ]
-  result ['happening'] = [ { 'date' : transaction ['time'], 
-                              'time' : random.choice (durations) } ]
+  result ['happening'] = [ { 'date' : transaction ['time'], 'time' : random.choice (durations) } ]
   result ['problem_type'] = [ random.choice (problem_types) ]
-  result ['comments'] = [ "test comment" ]
+  result ['comments'] = "test comment"
 
   print (result)
   return json.dumps (result)
@@ -167,21 +121,21 @@ def put_picture (transaction, sink) :
 
 def put_event (transaction, sink) :
   """Puts the dummy event record."""
-  template = """insert into bicycleparking_event ("sourceIP", "timeOf", distance, 
-                                                  answer_id, area_id) values
-                                                  (%(ip)s, %(time)s, %(distance)s, 
-                                                  %(answer)s, %(area)s);"""
+  template = """insert into bicycleparking_event ("sourceIP", "timeOf",
+                                                  answer_id) values
+                                                  (%(ip)s, %(time)s, %(answer)s);"""
   sink.execute (template, transaction)
   print (transaction)
 
 try:
-  to = psycopg2.connect ("dbname='{0}' host='{1}' user = '{2}' password = '{3}' port = 5435".format (SINK_DB, DB_HOST, DB_USER, DB_PW))
-
-  flushTestDB (to)
-  sink = to.cursor ()
-  to.commit ()
+  db = psycopg2.connect ("dbname='{0}' host='{1}' user = '{2}' password = '{3}' port = 5435".format (DB, DB_HOST, DB_USER, DB_PW))
+  
+  flushTestDB (db)
+  sink = db.cursor ()
+  construct ("test/transactions.xml", sink)
+  db.commit ()
   sink.close ()
-  to.close ()
+  db.close ()
 except psycopg2.Warning as warning:
   if warning.pgerror != None: 
      print (warning.pgerror)

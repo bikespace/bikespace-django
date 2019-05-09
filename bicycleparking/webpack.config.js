43adf0d18603d36ac0748cf6427ae2a965cdd9b6
j-rewerts <jaredrewerts@gmail.com>
2019-05-09 11:37:55 -0600

    Updated static asset bundling.

--- bicycleparking/webpack.config.js
+++ bicycleparking/webpack.config.js
@@ -1,26 +1,18 @@
-
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = env => {
-    console.log('STATIC_PATH', env.STATIC_PATH)
     return {
-        context: __dirname + "/static",
-        entry: './src/js/index.js',
+        context: __dirname,
+        entry: './static/src/js/index.js',
         output: {
-            path: path.join(__dirname, '/static/dist'),
-            publicPath: env.STATIC_PATH,
+            path: path.join(__dirname, env.BUILD_PATH || './dist'),
+            publicPath: env.ASSET_PATH || './',
             filename: 'bundle.js',
         },
-        plugins: [new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            templateParameters: {
-                'assetpath': env.STATIC_PATH
-            }
-        }), new CopyWebpackPlugin([
-            { from: 'src/style', to: 'style' },
-            { from: 'src/sw.js', to: 'sw.js' },
-            { from: 'src/manifest.json', to: 'manifest.json' }])],
+        plugins: [new CopyWebpackPlugin([
+            { from: 'static/src/style', to: 'style' }
+          ])
+        ],
     }
 }
